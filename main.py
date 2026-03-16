aa5e4b1631fd8034c6c857ae944a07fec853e1a8
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:04:57 +0700

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:04:45.280Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-16T05:04:55.397Z
+Commit: Fix CSS styling bug on mobile devices

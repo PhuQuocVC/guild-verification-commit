d85416b1837417f5153f778ae600a4d46e44f904
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:05:49 +0700

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:05:31.955Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-16T05:05:47.512Z
+Commit: Resolve timezone issues in date handling

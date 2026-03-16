75d55a7565876e530195a647e7979f326f96cdc5
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:07:52 +0700

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:07:24.852Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-16T05:07:50.490Z
+Commit: Patch API endpoint error handling

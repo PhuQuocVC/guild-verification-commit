3bab63497a6d92fb85475ccf5828a32e3026139b
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:07:32 +0700

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:07:15.810Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-16T05:07:29.638Z
+Commit: Resolve timezone issues in date handling

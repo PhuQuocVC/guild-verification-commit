63f22371c6f4670e6f9feb02b2a8f30853748a85
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:10:18 +0700

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:10:07.180Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-16T05:10:16.317Z
+Commit: Fix memory leak in event handlers

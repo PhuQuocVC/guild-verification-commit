c2fe2c55c901bac5cf024484075e68f9c7b15cde
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:05:56 +0700

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:05:47.512Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-16T05:05:54.682Z
+Commit: Patch security vulnerability in authentication

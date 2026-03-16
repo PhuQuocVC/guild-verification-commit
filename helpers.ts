eb8c34ab015595532335f4855e797969450c49ac
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:10:51 +0700

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:10:12.174Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-16T05:10:48.856Z
+Commit: Patch security vulnerability in authentication

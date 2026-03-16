bc45f450fca77bdb51411cb9e10ed6c6375a8af6
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:06:18 +0700

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:05:35.367Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-16T05:06:15.495Z
+Commit: Patch security vulnerability in authentication

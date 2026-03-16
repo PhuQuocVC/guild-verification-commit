565f594b4d20559ac1cc799b479f25c9bcf81d5c
PhuQuocVC <138224377+PhuQuocVC@users.noreply.github.com>
2026-03-16 12:12:10 +0700

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T05:12:03.017Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-16T05:12:08.078Z
+Commit: Fix broken links in documentation
